ca250e08820767ac50914a6f1199549d84b70189
ankitsharma125 <aankitsharma25@gmail.com>
2024-09-06 20:17:17 +0530

    Add files via upload
    This is a fully functional Connect 4 game using Java, JavaFX, and AWT, featuring a dynamic graphical user interface and interactive gameplay.

---
+++ Connect-4 Game/src/connect4package/Main.java
@@ -0,0 +1,103 @@
+package connect4package;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Controller controller;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("connect4.fxml"));
+        GridPane rootGridPane = loader.load();
+
+        controller = loader.getController();
+        controller.createPlayground();
+
+        MenuBar menuBar = createMenu();
+        menuBar.prefWidthProperty().bind(primaryStage.widthProperty());
+
+        Pane menuPane = (Pane) rootGridPane.getChildren().get(0);
+        menuPane.getChildren().add(menuBar);
+
+        Scene scene = new Scene(rootGridPane);
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Connect Four Game Packet");
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    private MenuBar createMenu() {
+
+        // File Menu
+        Menu fileMenu = new Menu("File");
+
+        MenuItem newGame = new MenuItem("New game");
+        newGame.setOnAction(event -> controller.resetGame());
+
+        MenuItem resetGame = new MenuItem("Reset game");
+        resetGame.setOnAction(event -> controller.resetGame());
+
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        MenuItem exitGame = new MenuItem("Exit game");
+        exitGame.setOnAction(event -> exitGame());
+
+        fileMenu.getItems().addAll(newGame, resetGame, separatorMenuItem, exitGame);
+
+        // Help Menu
+        Menu helpMenu = new Menu("Help");
+
+        MenuItem aboutGame = new MenuItem("About Connect4");
+        aboutGame.setOnAction(event -> aboutConnect4());
+
+        SeparatorMenuItem separator = new SeparatorMenuItem();
+        MenuItem aboutMe = new MenuItem("About Me");
+        aboutMe.setOnAction(event -> aboutMe());
+
+        helpMenu.getItems().addAll(aboutGame, separator, aboutMe);
+
+        MenuBar menuBar = new MenuBar();
+        menuBar.getMenus().addAll(fileMenu, helpMenu);
+
+        return menuBar;
+    }
+
+    private void aboutMe() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About The Developer");
+        alert.setHeaderText("Ankit Sharma");
+        alert.setContentText("This is my project for the Java at Internshala ");
+
+        alert.show();
+    }
+
+    private void aboutConnect4() {
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About Connect Four");
+        alert.setHeaderText("?");
+        alert.setContentText("Connect_Four_Game: Win After Connecting Four Same Colour Balls In Any Direction");
+
+        alert.show();
+    }
+
+    private void exitGame() {
+
+        Platform.exit();
+        System.exit(0);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
